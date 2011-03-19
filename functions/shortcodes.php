4bc855b444bea73242e6a5e37adc82000ba43d31
mikaeljorhult <mikael@mishkin.se>
2011-03-20 00:44:09 +0100

    Building as a class.

--- functions/shortcodes.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function plugin_boilerplate_shortcode($atts) {
-		extract(shortcode_atts(array(
-			'attribute' => 'default',
-		), $atts));
-	}
-	
-	add_shortcode('shortcode', 'plugin_boilerplate_shortcode');
-?>
