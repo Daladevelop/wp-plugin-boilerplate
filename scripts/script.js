fc4d4375615750c6cb23c9e5de636aa5422bc8a1
Rickard Andersson <rickard@montania.se>
2011-03-20 01:10:37 +0100

    Moved widget file Added anonymous self executing function to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($) {
 	// $() will work as an alias for jQuery() inside of this function
 });
+
+(function($) {
+	// Place your javascript in here
+	
+	// This is a self executing anonymous function which will keep your scripts from using up
+	// names in the global name space, and will prevent collisions with other scripts. 
+	
+	// $() will work as an alias for jQuery() inside of this function 
+})(jQuery);
